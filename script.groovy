d84f7c83d20b9603792959ffcd0cbad0721464fe
Atiyaali <atiyaali653@gmail.com>
2026-06-10 19:07:02 +0500

    jenkins added

---
+++ script.groovy
@@ -0,0 +1,18 @@
+def build(frontimage , backimage , nginximage , frontpath , backpath , nginxpath){
+ sh "docker build -f ${frontpath} -t ${frontimage} ."
+  sh "docker build -f ${backpath} -t ${backimage} ."
+   sh "docker build -f ${nginxpath} -t ${nginximage} ."
+}
+
+def push(frontimage , backimage , nginximage){
+    withCredentials([
+ usernamePassword(  credentialsID: "dockerhub_creds" , usernameVariable: 'USERNAME' , passwordVariable: 'PASS')
+  
+    ]){
+        sh "echo ${PASS} | docker login -u ${USERNAME} --passwordstdin"
+    }
+
+sh "docker push ${frontimage}"
+sh "docker  push ${backimage}"
+sh "docker push ${nginximage}"
+}
